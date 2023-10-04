0946240a77c9348c972010dc9eeb18096ed2cc06
JoshuaSVU <115957792+JoshuaSVU@users.noreply.github.com>
2023-10-04 14:08:03 -0500

    Test Pages

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,6 +4,11 @@ function Home(){
     return(
         <div>
             <h1> Welcome to the Bank</h1>
+            <ul>
+                <li><Link to="/Register">Register for an account.</Link></li>
+                <li><Link to="/Login">Login to your account.</Link></li>
+                <li><Link to="/ForgotPass">Forgot your password?</Link></li>
+            </ul>
         </div>
     )
 }
---
+++ client/src/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register(){
+    return(
+        <div>
+            <h1> Register for an account.</h1>
+        </div>
+    )
+}
+
+export default Register;
