a82c6e64d2dbb76cc8cf06bda4cbdcdbb06753d6
aeruny <67850318+aeruny@users.noreply.github.com>
2023-10-09 01:24:33 -0500

    added API calls for login and customer features

--- server/database.js
+++ server/database.js
@@ -8,70 +8,207 @@ const dbKey = process.env.SUPABASE_PUBLIC_API_KEY; // Setting db API key
 const supabase = createClient(dbUrl, dbKey);
 
 
-// ---------------- Get ---------------------
-// 
-async function getData() {
-    const { data, error } = await supabase.from('User').select('*');
+const debugging = true;
 
-    return { data, error };
+// ---------------- User Login -------------
+// request for UserID
+async function getUserID(email, password) {
+    const { data, error } = await supabase
+    .from('User')
+    .select('UserID')
+    .eq('Email', email)
+    .eq('Password', password);
+    if (error) {
+        throw error;
+    }
 
-};
+    // logActivity("User", "NULL", "GET UserID", "", true)
+    return data;
+}
 
-async function getUserInformation(email) {
+async function getUserLogin(userID) {
+    const { data, error } = await supabase
+    .from('User')
+    .select('Email, Password')
+    .eq('UserID', userID);
+
+    if (error) {
+        throw error;
+    }
+
+    // logActivity("User", "NULL", "GET UserID", "", true)
+    return data;
+}
+
+// Get User Information
+// Params: UserID
+// Return: the user's personal information
+async function getUserInformation(userID) {
     const { data, error } = await supabase
         .from('User')
         .select('Role, FirstName, LastName, Address, PhoneNumber, SSN, DOB')
-        .eq('Email', email);
-        
-    return { data, error };
+        .eq('UserID', userID);
+    
+    if (error) {
+        throw error;
+    }
+
+    return data;
 }
 
-// Get User Account Information
-// Params: UserID
-// Return: Account
-async function getAccountInformation(userID) {
-    let { data, error } = await supabase.from('Account').select('*').eq("UserID", userID);
 
-    return { data, error };
-}
-
-// Get Customer Account Types
+// Get Accounts List
 // Params: UserID
-// Return: { AccountType: "Checking" | "Savings" | "Money Market" | "Home Mortgage Loan" | "Credit Card" } 
-async function getAccountNames(userID) {
+// Return: list of all accounts the customer has 
+async function getAccountsList(userID) {
     let { data, error } = await supabase
     .from('Account')
     .select('AccountType, AccountName')
     .eq('UserID', userID);
 
-    return { data, error };
+    if (error) {
+        throw error;
+    }
+
+    return data;
 }
 
+// Get User Account Information
+// Params: UserID, AccountID
+// Return: the account's information
+async function getAccountInformation(userID, accountID) {
+    let { data, error } = await supabase
+    .from('Account')
+    .select('*')
+    .eq('UserID', userID)
+    .eq('AccountID', accountID);
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
+
+
+
+// -------------------------------- Administrator ----------------------------
+
+// Get Tellers List
+// params: None
+// return: a list of tellers {Email, FirstName, LastName}
+async function getTellersList() {
+    const { data, error } = await supabase
+    .from('User')
+    .select('Email, FirstName, LastName')
+    .eq('Role', 'Teller');
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
+
+// Get Teller Information
+async function getTellerInformation(userID) {
+    const { data, error } = await supabase
+    .from('User')
+    .select('Email, FirstName, LastName')
+    .eq('UserID', userID);
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
+
+// Add a Teller
+// params: email, password, firstName, lastName
+// return: a list of tellers {Email, FirstName, LastName}
+async function addTeller(email, password, firstName, lastName) {
+    const { data, error } = await supabase
+    .from('User')
+    .insert([
+        { 'Role': "Teller", 'Email': email, 'Password': password, 'FirstName': firstName, 'LastName': lastName
+     }
+    ])
+    .select();
+    
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    
+    return data;
+}
+
+async function updateTeller(email, password, firstName, lastName) {
+    const { data, error } = await supabase
+    .from('User')
+    .update([
+        { 'Role': "Teller", 'Email': email, 'Password': password, 'FirstName': firstName, 'LastName': lastName
+     }
+    ])
+    .select();
+    
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    
+    return data;
+}
+
+async function deleteUser(userID) {
+    const { data, error } = await supabase
+    .from('User')
+    .delete()
+    .eq('UserID', userID);
+    
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    
+    return data;
+}
+
+
 // --------------------------------- Log -----------------------------------
-async function addLog(userID, activityType, activityDetail) {
+async function logActivity(refTable, refID, activityType, activityDetail, successful) {
+    if (debugging) {
+        return
+    }
+
     const { data, error } = await supabase
         .from('Log')
         .insert([
         {
-            UserID: userID,
-            ActivityType: activityType,
-            ActivityDetail: activityDetail,
-            Timestamp: new Date().toISOString(), // Use 'created_at' for the timestamp
+            "ReferenceTable": refTable,
+            "ReferenceID": refID,
+            "ActivityType": activityType,
+            "ActivityDetail": activityDetail,
+            "Timestamp": new Date().toISOString(), // Use 'created_at' for the timestamp
+            "IsSuccessful": successful
         },
     ]);
-    
     if (error) {
       throw error;
     }
-  
     return data;
   }
   
 
 module.exports = {
-    getData,
+    getUserID,
     getUserInformation,
+    deleteUser,
+    getAccountsList,
     getAccountInformation,
-    getAccountNames,
-    addLog
+    getTellersList,
+    addTeller,
+    updateTeller,
+    logActivity
 }
