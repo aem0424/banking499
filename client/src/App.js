b4c161f63c3595e9c62abde0d5c84815b9ab0b36
aem0424 <aem0025@uah.edu>
2023-10-16 16:39:44 -0500

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -35,26 +35,25 @@ import TellerEditAccount from ".//teller/TellerEditAccount";
 import AdminMain from './/admin/AdminMain'
 import AdminTellerList from '../admin/AdminTellerList'
 import {Route, Routes} from 'react-router-dom';
-//import Url from "./Url.js";
+import Url from "./Url.js";
 
 
 function App() {
   return (
    <div className="App" >
     <Routes>
-      {/* <Route exact path={Constants.HOME} element ={<Home/>}/> <- this is experimental and a wip*/}
-      <Route exact path="/" element={<Home/>}/>
+      <Route exact path={Url.H_HOME} element ={<Home/>}/>
       <Route exact path="/DataDisplay" element ={<DataDisplay/>}/>
       <Route exact path="/reactdemo" element ={<ReactDemo/>}/>
       <Route exact path="/ForgotPass" element ={<ForgotPass/>}/>
-      <Route exact path="/Login" element ={<Login/>}/>
-      <Route exact path="/Register" element ={<Register/>}/>
-      <Route exact path="/customer" element ={<CustomerMain/>}/>
-      <Route exact path="/customer/paybill" element={<CustomerBillPay/>}/>
-      <Route exact path="/customer/account" element={<CustomerViewAccountList/>}/>
-      <Route exact path="/customer/account/view" element={<CustomerViewAccountInformation/>}/>
-      <Route exact path="/customer/account/create" element={<CustomerCreateAccount/>}/>
-      <Route exact path="/customer/user" element={<CustomerViewUserInformation/>}/>
+      <Route exact path={Url.H_LOGIN} element ={<Login/>}/>
+      <Route exact path={Url.H_REGISTER} element ={<Register/>}/>
+      <Route exact path={Url.C_MAIN} element ={<CustomerMain/>}/>
+      <Route exact path={Url.C_BILLPAY} element={<CustomerBillPay/>}/>
+      <Route exact path={Url.C_ACCOUNT} element={<CustomerViewAccountList/>}/>
+      <Route exact path={Url.C_VIEW} element={<CustomerViewAccountInformation/>}/>
+      <Route exact path={Url.C_CREATE} element={<CustomerCreateAccount/>}/>
+      <Route exact path="/customer" element={<CustomerViewUserInformation/>}/>
       <Route exact path="/customer/transaction" element={<CustomerTransaction/>}/>
       <Route exact path="/customer/transaction/transfer" element={<CustomerTransfer/>}/>
       <Route exact path="/customer/transaction/deposit" element={<CustomerDeposit/>}/>
