966159caff0b0a33dd7769ba11eff7d772135109
aeruny <67850318+aeruny@users.noreply.github.com>
2023-11-09 16:29:02 -0600

    backend: add bcrypt for encryption

--- server/encryption.js
+++ server/encryption.js
@@ -1,11 +1,19 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // The cost factor for the bcrypt algorithm
 
+// Password from the user's input
+const userPassword = req.body.Password;
 
-
-function hash(value) {
-
-    return hashedValue
-}
-
+// Hash the password
+bcrypt.hash(userPassword, saltRounds, (err, hash) => {
+    if (err) {
+        // Handle the error
+    } else {
+        // Store the 'hash' in the database
+        const hashedPassword = hash;
+        // Save 'hashedPassword' in your database
+    }
+});
 
 
 module.exports = {
