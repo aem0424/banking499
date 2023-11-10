8c05df2e87fa22c833d4097418b6f4be6027668c
aeruny <67850318+aeruny@users.noreply.github.com>
2023-11-10 02:32:18 -0600

    fixed typo

--- server/src/teller.js
+++ server/src/teller.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const database = require('./database.js');
-f
+
 // ------------------------ Teller -----------------------------------
 
 // GET: Get Teller Information (Login Required)
