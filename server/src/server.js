4718d7b9eae2032172f5b3931cd506788887bf46
aeruny <67850318+aeruny@users.noreply.github.com>
2023-11-21 20:39:19 -0600

    backend: add some get password calls

--- server/src/server.js
+++ server/src/server.js
@@ -229,13 +229,30 @@ app.post('/user/logout', async (req, res) => {
   });
 });
 
+// GET: Get a User's Password
+// Params: Email
+// Return: String (Password)
+app.get('/user/password', async (req, res) => {
+  console.log(`Getting a User's Password`);
+
+  email = req.query.Email;
+  // Query User Information
+  let [userPassword, err_userPassword] = await database.getUserPassword(email);
+  if (err_userPassword) return res.status(500).json({ error: `Failed to Query User ${userID}'s Address`, message: err_userData.message });
+
+
+  let password = userPassword[0];
+  return res.status(200).json(password);
+});
+
+
 // GET: Get the Current User's Password (Login Required)
 // Params: None
 // Return: String (Password)
-app.get('/user/password', async (req, res) => {
+app.get('/user/password/current', async (req, res) => {
   let userID = req.session.user?.UserID;
   let userEmail = req.session.user?.Email;
-  console.log(`Getting User ${userID}'s Role`);
+  console.log(`Getting User ${userID}'s Password`);
   if (!userID) return res.status(401).json({ error: "User Is Not Logged In" });
 
   // Query User Information
